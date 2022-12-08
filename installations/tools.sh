f0a9e6c4905f365987970ddcb016dd2849e7cf24
Yony6041 <dercomputerschamane@gmail.com>
2022-12-07 18:08:51 -0600

    File to download dependencies

---
+++ installations/tools.sh
@@ -0,0 +1,9 @@
+printf "Downloading bioinformatic tools"
+cd ../dependencies
+wget https://github.com/s-andrews/FastQC/archive/refs/tags/v0.11.9.tar.gz
+tar -xvf v0.11.9.tar.gz
+rm v0.11.9.tar.gz
+
+wget https://github.com/ewels/MultiQC/archive/refs/tags/v1.13.tar.gz
+tar -xvf v1.13.tar.gz
+rm v1.13.tar.gz
