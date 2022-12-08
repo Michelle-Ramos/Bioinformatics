91cd65a548b33cf4ecea10a39c36c4f13639a502
Yony6041 <dercomputerschamane@gmail.com>
2022-12-08 16:59:17 -0600

    Getting sample data & reference genome

--- installations/tools.sh
+++ installations/tools.sh
@@ -9,3 +9,22 @@ tar -xvf v1.13.tar.gz
 rm v1.13.tar.gz
 cd ../
 curl -s https://get.nextflow.io | bash
+
+# Getting sample RNA-seq data for testing FastQC && MultiQC 
+printf "Getting sample RNA-seq data for testing FastQC && MultiQC"
+
+cd samples/rawData/
+mkdir RNA-seq
+cd RNA-seq
+wget http://genomedata.org/rnaseq-tutorial/HBR_UHR_ERCC_ds_5pc.tar
+tar -xf HBR_UHR_ERCC_ds_5pc.tar
+rm HBR_UHR_ERCC_ds_5pc.tar
+
+# Download the reference genome "Cicero_reference" was used
+printf "Download the reference genome Cicero_reference was used"
+cd ..
+mkdir references
+cd references
+wget https://zenodo.org/record/5088371/files/reference.tar.gz
+tar -xvf reference.tar.gz
+rm reference.tar.gz
