3ae78da15256d9f662f6d9cb45c32e81b75b1da9
Yony6041 <dercomputerschamane@gmail.com>
2022-12-08 17:30:29 -0600

    Hotfix: Dependencies directory

--- installations/tools.sh
+++ installations/tools.sh
@@ -1,5 +1,7 @@
 printf "Downloading bioinformatic tools"
-cd ../dependencies
+cd ../
+mkdir dependencies
+cd dependencies
 wget https://github.com/s-andrews/FastQC/archive/refs/tags/v0.11.9.tar.gz
 tar -xvf v0.11.9.tar.gz
 rm v0.11.9.tar.gz
@@ -7,6 +9,7 @@ rm v0.11.9.tar.gz
 wget https://github.com/ewels/MultiQC/archive/refs/tags/v1.13.tar.gz
 tar -xvf v1.13.tar.gz
 rm v1.13.tar.gz
+
 cd ../
 curl -s https://get.nextflow.io | bash
 
